635320fb30f510056f948b26494a1a9aa32a6335
Jayesh Sharma <wjayesh@outlook.com>
2025-01-19 10:05:30 +0530

    expose common classes directly

--- mahilo/__init__.py
+++ mahilo/__init__.py
@@ -1,7 +1,8 @@
-# from .agent import BaseAgent
-# from .agent_manager import AgentManager
-# from .session import Session
-# from .server import ServerManager
-# from .client import Client
+from .agent import BaseAgent
+from .agent_manager import AgentManager
+from .session import Session
+from .server import ServerManager
+from .client import Client
+from .cli import cli
 
-# __all__ = ["BaseAgent", "AgentManager", "Session", "ServerManager", "Client"]
+__all__ = ["BaseAgent", "AgentManager", "Session", "ServerManager", "Client", "cli"]
